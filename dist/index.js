62acdd1a80bd2c6cf18ffc884da2e5ad73b4ec1b
Andre Thomas <andrethomas6@yahoo.com>
2020-11-06 14:59:12 -0800

    Update player and enemy styles. Added function to check collisions and end game.

--- dist/index.js
+++ dist/index.js
@@ -12,3 +12,18 @@ function jump() {
     }, 1000);
   }
 }
+
+let checkCollision = setInterval(() => {
+  let playerTop = parseInt(
+    window.getComputedStyle(player).getPropertyValue("top")
+  );
+  let enemyLeft = parseInt(
+    window.getComputedStyle(enemy).getPropertyValue("left")
+  );
+
+  if (enemyLeft < 35 && enemyLeft > -30 && playerTop > 375) {
+    enemy.style.animation = "none";
+    enemy.style.display = "none";
+    alert(" You Lose!!! ");
+  }
+}, 10);
